f2c1e2831cc38837f3856901a947d9f96c3642ec
Theophile Wallez <theophile@wallez.net>
2015-07-31 16:05:49 +0200

    Use Eigen to improve performance with vectorization (10x faster)

--- src/FirstModel.cpp
+++ src/FirstModel.cpp
@@ -23,41 +23,30 @@ std::vector<int> FirstModel::wordsToIndices(const std::vector<std::string>& sent
 FirstModel::FirstModel(int n, float lambda) :
     _n(n),
     _lambda(lambda),
-    _theta(4*n, std::vector<float>(n, 0.0)) {
+    _theta(Eigen::MatrixXf::Zero(4*_n, _n)) {
 
     std::mt19937 gen;
     std::normal_distribution<float> dist(RANDOM_MEAN, RANDOM_STDDEV);
     for(int i = 0; i < 4*n; ++i) {
         for(int j = 0; j < n; ++j) {
-            _theta[i][j] = dist(gen);
+            _theta(i, j) = dist(gen);
         }
     }
 }
 
 void FirstModel::train(const std::vector<std::string>& sentence, float alpha) {
     std::vector<int> indices = wordsToIndices(sentence);
-    std::vector<std::vector<float>> dtheta(4*_n,std::vector<float>(_n, 0.0));
-    std::vector<std::vector<float>> dword(sentence.size(), std::vector<float>(_n, 0.0));
+    Eigen::MatrixXf dtheta(4*_n, _n);
+    std::vector<Eigen::VectorXf> dword(sentence.size(), Eigen::VectorXf::Zero(_n));
     for(int i = 0; i<4*_n; i++) {
-        for(int j = 0; j<_n; j++) {
-            dtheta[i][j] = derivTheta(i, j, indices);
-        }
+        dtheta.row(i) = derivTheta(i, indices).transpose();
     }
     for(int i = 0; i < sentence.size()-4; i++) {
-        for(int k = 0; k<_n; k++) {
-            dword[i][k] = derivWord(i, k, indices);
-        }
+        dword[i] = derivWord(i, indices);
     }
-
-    for (int i = 0; i<4*_n; i++) {
-        for (int j = 0; j<_n; j++) {
-            _theta[i][j] -= dtheta[i][j]*alpha;
-        }
-    }
+    _theta -= dtheta*alpha;
     for (int i = 0; i<sentence.size(); i++) {
-        for (int j = 0; j<_n; j++) {
-            _indtovec[indices[i]][j] -= dword[i][j]*alpha;
-        }
+        _indtovec[indices[i]] -= dword[i]*alpha;
     }
 }
 
@@ -70,19 +59,10 @@ int FirstModel::vocabSize() {
 }
 
 
-float FirstModel::hypothesis(int j, const std::vector<int>& phrase) {
-    float S = 0;
-    for (int l = 0; l < 4*_n; l++) {
-        float valthet = _theta[l][j];
-        int c = l/_n;
-        if (c>=2) {
-            c++;
-        }
-        int k = l%_n;
-        float valvec = _indtovec[phrase[c]][k];
-        S += valthet*valvec;
-    }
-    return S;
+Eigen::VectorXf FirstModel::hypothesis(const std::vector<int>& phrase) {
+    Eigen::VectorXf words(4*_n);
+    words << _indtovec[phrase[0]], _indtovec[phrase[1]], _indtovec[phrase[3]], _indtovec[phrase[4]];
+    return _theta.transpose()*words;
 }
 
 float FirstModel::error(const std::vector<int>& sentence) {
@@ -96,7 +76,7 @@ float FirstModel::error(const std::vector<int>& sentence) {
     //Regularization
     for(int j = 0; j < _n; ++j) {
         for (int l = 0; l<4*_n; l++) {
-            S += _lambda/2 * carre(_theta[l][j]);
+            S += _lambda/2 * carre(_theta(l, j));
         }
         for (int i = 0; i<sentence.size(); i++) {
             S += _lambda/2 * carre(_indtovec[sentence[i]][j]);
@@ -105,24 +85,24 @@ float FirstModel::error(const std::vector<int>& sentence) {
     return S;
 }
 
-float FirstModel::derivTheta(int l, int j, const std::vector<int>& sentence) {
-    float res = 0;
+Eigen::VectorXf FirstModel::derivTheta(int l, const std::vector<int>& sentence) {
+    Eigen::VectorXf res = Eigen::VectorXf::Zero(_n);
     for(int i = 0; i < sentence.size()-4; ++i) {
         std::vector<int> example(sentence.begin()+i, sentence.begin()+i+5);
-        res += derivThetaEx(l, j, example);
+        res += derivThetaEx(l, example);
     }
-    return res/(sentence.size()-4)+ _lambda*_theta[l][j];
+    return res/(sentence.size()-4)+ _lambda*_theta.row(l).transpose();
 }
 
-float FirstModel::derivWord(int i, int k, const std::vector<int>& sentence) {
-    float res = 0;
+Eigen::VectorXf FirstModel::derivWord(int i, const std::vector<int>& sentence) {
+    Eigen::VectorXf res = Eigen::VectorXf::Zero(_n);
     for(int c = 0; c < 5; c++) {
         if(i-c >= 0 && i-c+5 <= sentence.size()) {
             std::vector<int> example(sentence.begin()+i-c, sentence.begin()+i-c+5);
-            res += derivWordEx(c, k, example);
+            res += derivWordEx(c, example);
         }
     }
-    return res/(sentence.size()-4) + _lambda*_indtovec[sentence[i]][k];
+    return res/(sentence.size()-4) + _lambda*_indtovec[sentence[i]];
 }
 
 int FirstModel::getWordInd(const std::string& word) {
@@ -132,11 +112,11 @@ int FirstModel::getWordInd(const std::string& word) {
     } else {
         int id = _indtovec.size();
         _wordtoind.emplace(word, id);
-        std::vector<float> vec;
+        Eigen::VectorXf vec(_n);
         std::mt19937 gen;
         std::normal_distribution<float> dist(RANDOM_MEAN, RANDOM_STDDEV);
         for(int i = 0; i < _n; ++i) {
-            vec.push_back(dist(gen));
+            vec(i) = dist(gen);
         }
         _indtovec.push_back(vec);
         return id;
@@ -148,14 +128,14 @@ void FirstModel::gradCheck(const std::vector<int>& sentence) {
     printf("Theta:\n");
     for(int l = 0; l < 4*_n; ++l) {
         for(int j = 0; j < _n; ++j) {
-            float theta = _theta[l][j];
-            _theta[l][j] = theta + epsilon;
+            float theta = _theta(l, j);
+            _theta(l, j) = theta + epsilon;
             float errpe = error(sentence);
-            _theta[l][j] = theta - epsilon;
+            _theta(l, j) = theta - epsilon;
             float errme = error(sentence);
-            _theta[l][j] = theta;
+            _theta(l, j) = theta;
             float approx = (errpe-errme)/(2*epsilon);
-            float calc = derivTheta(l, j, sentence);
+            float calc = derivTheta(l, sentence)(j);
             printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, l, j);
         }
     }
@@ -170,7 +150,7 @@ void FirstModel::gradCheck(const std::vector<int>& sentence) {
             float errme = error(sentence);
             _indtovec[sentence[c]][k] = word;
             float approx = (errpe-errme)/(2*epsilon);
-            float calc = derivWord(c, k, sentence);
+            float calc = derivWord(c, sentence)(k);
             printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, c, k);
         }
         printf("\n");
@@ -180,27 +160,23 @@ void FirstModel::gradCheck(const std::vector<int>& sentence) {
 float FirstModel::errorEx(const std::vector<int>& example) {
     float S = 0;
     for (int j = 0; j<_n; j++) {
-        S += carre(hypothesis(j,example) - _indtovec[example[2]][j]);
+        S += carre(hypothesis(example)(j) - _indtovec[example[2]][j]);
     }
     return S/2;
 }
 
-float FirstModel::derivThetaEx(int l, int j, const std::vector<int>& example) {
+Eigen::VectorXf FirstModel::derivThetaEx(int l, const std::vector<int>& example) {
     int c = l/_n;
     c += (c>=2);
     int k = l%_n;
-    return (hypothesis(j, example)-_indtovec[example[2]][j])*_indtovec[example[c]][k];
+    return (hypothesis(example) -_indtovec[example[2]])*_indtovec[example[c]][k];
 }
 
-float FirstModel::derivWordEx(int c, int k, const std::vector<int>& example) {
+Eigen::VectorXf FirstModel::derivWordEx(int c, const std::vector<int>& example) {
     if (c == 2) {
-        return _indtovec[example[c]][k]-hypothesis(k, example);
+        return _indtovec[example[c]]-hypothesis(example);
     } else {
-        float S = 0;
-        for (int j=0; j < _n; j++) {
-            S += _theta[(c-(c>2))*_n+k][j]*(hypothesis(j,example)-_indtovec[example[2]][j]);
-        }
-        return S;
+        return _theta.block((c-(c>2))*_n, 0, _n, _n) * (hypothesis(example) - _indtovec[example[2]]);
     }
 }
 
@@ -210,14 +186,14 @@ void FirstModel::gradCheckEx(const std::vector<int>& example) {
     printf("Theta:\n");
     for(int l = 0; l < 4*_n; ++l) {
         for(int j = 0; j < _n; ++j) {
-            float theta = _theta[l][j];
-            _theta[l][j] = theta + epsilon;
+            float theta = _theta(l, j);
+            _theta(l, j) = theta + epsilon;
             float errpe = errorEx(example);
-            _theta[l][j] = theta - epsilon;
+            _theta(l, j) = theta - epsilon;
             float errme = errorEx(example);
-            _theta[l][j] = theta;
+            _theta(l, j) = theta;
             float approx = (errpe-errme)/(2*epsilon);
-            float calc = derivThetaEx(l, j, example);
+            float calc = derivThetaEx(l, example)(j);
             printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, l, j);
         }
     }
@@ -232,7 +208,7 @@ void FirstModel::gradCheckEx(const std::vector<int>& example) {
             float errme = errorEx(example);
             _indtovec[example[c]][k] = word;
             float approx = (errpe-errme)/(2*epsilon);
-            float calc = derivWordEx(c, k, example);
+            float calc = derivWordEx(c, example)(k);
             printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, c, k);
         }
         printf("\n");
--- src/FirstModel.h
+++ src/FirstModel.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <Eigen/Dense>
 #include "Model.h"
 
 class FirstModel : public Model {
@@ -13,25 +14,25 @@ class FirstModel : public Model {
         virtual float error(const std::vector<std::string>& sentence);
         virtual int vocabSize();
 
-        float hypothesis(int j, const std::vector<int>& phrase);
+        Eigen::VectorXf hypothesis(const std::vector<int>& phrase);
         float error(const std::vector<int>& sentence);
-        float derivTheta(int l, int j, const std::vector<int>& sentence);
-        float derivWord(int i, int k, const std::vector<int>& sentence);
+        Eigen::VectorXf derivTheta(int l, const std::vector<int>& sentence);
+        Eigen::VectorXf derivWord(int i, const std::vector<int>& sentence);
         int getWordInd(const std::string& word);
         std::vector<int> wordsToIndices(const std::vector<std::string>& sentence);
         void gradCheck(const std::vector<int>& phrase);
 
         //Functions working on a single example (5 words) without regularization
         float errorEx(const std::vector<int>& example);
-        float derivThetaEx(int l, int j, const std::vector<int>& example);
-        float derivWordEx(int c, int k, const std::vector<int>& example);
+        Eigen::VectorXf derivThetaEx(int l, const std::vector<int>& example);
+        Eigen::VectorXf derivWordEx(int c, const std::vector<int>& example);
         void gradCheckEx(const std::vector<int>& example);
 
     private:
         int _n;
         float _lambda;
-        std::vector<std::vector<float>> _theta;
-        std::vector<std::vector<float>> _indtovec;
+        Eigen::MatrixXf _theta;
+        std::vector<Eigen::VectorXf> _indtovec;
         std::unordered_map<std::string, int> _wordtoind;
 };
 
