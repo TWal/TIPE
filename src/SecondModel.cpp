4427c30904906cd5368d009ab14c08dca280e75f
Theophile Wallez <theophile@wallez.net>
2015-10-24 00:54:35 +0200

    Beginning of the second model

---
+++ src/SecondModel.cpp
@@ -0,0 +1,175 @@
+#include "SecondModel.h"
+
+#include <cmath>
+#include <cfloat>
+
+static const float RANDOM_MEAN = 0.0;
+static const float RANDOM_STDDEV = 1.0;
+
+static float sigmoid(float x) {
+    return 1/(1+exp(-x));
+    // 1/2 + 1/2tanh(x/2)
+}
+
+SecondModel::SecondModel(int n, int v) :
+    _n(n), _v(v) {
+
+    std::normal_distribution<float> dist(RANDOM_MEAN, RANDOM_STDDEV);
+    std::mt19937 gen;
+
+    for(int i = 0; i < _v; ++i) {
+        Eigen::VectorXf vec(_n);
+        _w1.push_back(Eigen::VectorXf::Zero(_n));
+        _w2.push_back(Eigen::VectorXf::Zero(_n));
+        for(int j = 0; j < _n; ++j) {
+            _w1[i](j) = dist(gen);
+            _w2[i](j) = dist(gen);
+        }
+    }
+}
+
+void SecondModel::train(const std::vector<std::string>& sentence, float alpha) {
+    //TODO
+}
+
+int SecondModel::sentenceSize() {
+    return SecondModel::CTX_SIZE+1;
+}
+
+void SecondModel::save(const std::string& file) {
+    //TODO
+}
+
+void SecondModel::load(const std::string& file) {
+    //TODO
+}
+
+void SecondModel::displayState(const std::vector<std::string>& sentence) {
+    std::array<int, SecondModel::CTX_SIZE> ctx;
+    int answer;
+    sentenceToContext(sentence, ctx, answer);
+    printf("error negsample: %f\terror softmax:%f\twords: %d\n", errorNegSample(ctx, answer), errorSoftmax(ctx, answer), _wordtoind.size());
+}
+
+Eigen::VectorXf SecondModel::hidden(const std::array<int, SecondModel::CTX_SIZE>& ctx) {
+    Eigen::VectorXf h = Eigen::VectorXf::Zero(_n);
+    for(int i = 0; i < SecondModel::CTX_SIZE; ++i) {
+        h += _w1[ctx[i]];
+    }
+    return h / SecondModel::CTX_SIZE;
+}
+
+float SecondModel::hypothesis(const Eigen::VectorXf& h, int i) {
+    return h.dot(_w2[i]);
+}
+
+float SecondModel::error(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok) {
+    Eigen::VectorXf h = hidden(ctx);
+    if(ok) {
+        return -log(sigmoid(hypothesis(h, i)));
+    } else {
+        return -log(sigmoid(-hypothesis(h, i)));
+    }
+}
+
+float SecondModel::errorNegSample(const std::array<int, SecondModel::CTX_SIZE>& ctx, int answer) {
+    float result = error(ctx, answer, true);
+    std::vector<int> sample = negSample(answer);
+    for(int i : sample) {
+        result += error(ctx, i, false);
+    }
+    return result/(sample.size()+1);
+}
+
+float SecondModel::errorSoftmax(const std::array<int, SecondModel::CTX_SIZE>& ctx, int answer) {
+    Eigen::VectorXf h = hidden(ctx);
+    std::vector<double> res;
+    for(int i = 0; i < _v; ++i) {
+        res.push_back(hypothesis(h, i));
+    }
+    double max = *std::max_element(res.begin(), res.end());
+    double sum = 0;
+    for(int i = 0; i < _v; ++i) {
+        res[i] = exp(res[i] - max);
+        sum += res[i];
+    }
+    return -log(res[answer]/sum);
+}
+
+
+Eigen::VectorXf SecondModel::derivW1(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok) {
+    Eigen::VectorXf h = hidden(ctx);
+    Eigen::VectorXf eh = (sigmoid(hypothesis(h, i))-ok)*_w2[i];
+    return eh/CTX_SIZE;
+}
+
+Eigen::VectorXf SecondModel::derivW2(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok) {
+    Eigen::VectorXf h = hidden(ctx);
+    return (sigmoid(hypothesis(h, i))-ok)*h;
+}
+
+void SecondModel::gradCheck(const std::array<int, SecondModel::CTX_SIZE>& ctx, int j, int k) {
+    float epsilon = sqrt(FLT_EPSILON);
+    for(int b = 0; b < 2; ++b) {
+        for(int i = 0; i < _n; ++i) {
+            float tmp = _w1[ctx[j]](i);
+            _w1[ctx[j]](i) = tmp + epsilon;
+            float errpe = error(ctx, k, b);
+            _w1[ctx[j]](i) = tmp - epsilon;
+            float errme = error(ctx, k, b);
+            _w1[ctx[j]](i) = tmp;
+            float approx = (errpe-errme)/(2*epsilon);
+            float calc = derivW1(ctx, k, b)(i);
+            printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, i, b);
+        }
+        for(int i = 0; i < _n; ++i) {
+            float tmp = _w2[k](i);
+            _w2[k](i) = tmp + epsilon;
+            float errpe = error(ctx, k, b);
+            _w2[k](i) = tmp - epsilon;
+            float errme = error(ctx, k, b);
+            _w2[k](i) = tmp;
+            float approx = (errpe-errme)/(2*epsilon);
+            float calc = derivW2(ctx, k, b)(i);
+            printf("\t%f\t%f\t%f\t%f\t%d\t%d\n", calc-approx, calc/approx, calc, approx, i, b);
+        }
+    }
+}
+
+void SecondModel::sentenceToContext(const std::vector<std::string>& sentence, std::array<int, SecondModel::CTX_SIZE>& ctx, int& answer) {
+    int j = 0;
+    for(int i = 0; i < sentence.size(); ++i) {
+        if(i == sentence.size()/2) {
+            answer = getWordInd(sentence[i]);
+        } else {
+            ctx[j] = getWordInd(sentence[i]);
+            ++j;
+        }
+    }
+}
+
+std::vector<int> SecondModel::negSample(int word) {
+    std::uniform_int_distribution<int> dist = std::uniform_int_distribution<int>(0, _v-2);
+    std::vector<int> result;
+    for(int i = 0; i < 10; ++i) {
+        int nb = dist(_gen);
+        if(nb >= word) {
+            nb += 1;
+        }
+        result.push_back(nb);
+    }
+    return result;
+}
+
+int SecondModel::getWordInd(const std::string& word) {
+    auto it = _wordtoind.find(word);
+    if(it != _wordtoind.end()) {
+        return it->second;
+    } else {
+        int id = _wordtoind.size();
+        _wordtoind.emplace(word, id);
+        return id;
+    }
+}
+
+
---
+++ src/SecondModel.h
@@ -0,0 +1,43 @@
+#ifndef SECONDMODEL_H
+#define SECONDMODEL_H
+
+#include <vector>
+#include <string>
+#include <Eigen/Dense>
+#include <unordered_map>
+#include <random>
+#include <array>
+#include "Model.h"
+
+class SecondModel : public Model {
+    public:
+        SecondModel(int n, int v);
+        virtual void train(const std::vector<std::string>& sentence, float alpha);
+        virtual int sentenceSize();
+        virtual void save(const std::string& file);
+        virtual void load(const std::string& file);
+        virtual void displayState(const std::vector<std::string>& sentence);
+
+        static const int CTX_SIZE = 4;
+        Eigen::VectorXf hidden(const std::array<int, SecondModel::CTX_SIZE>& ctx);
+        float hypothesis(const Eigen::VectorXf& h, int i);
+        float error(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok);
+        float errorNegSample(const std::array<int, SecondModel::CTX_SIZE>& ctx, int answer);
+        float errorSoftmax(const std::array<int, SecondModel::CTX_SIZE>& ctx, int answer);
+        Eigen::VectorXf derivW1(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok);
+        Eigen::VectorXf derivW2(const std::array<int, SecondModel::CTX_SIZE>& ctx, int i, bool ok);
+        void gradCheck(const std::array<int, SecondModel::CTX_SIZE>& ctx, int j, int k);
+        void sentenceToContext(const std::vector<std::string>& sentence, std::array<int, SecondModel::CTX_SIZE>& ctx, int& answer);
+        std::vector<int> negSample(int word);
+        int getWordInd(const std::string& word);
+    private:
+        int _n;
+        int _v;
+        std::vector<Eigen::VectorXf> _w1;
+        std::vector<Eigen::VectorXf> _w2;
+        std::unordered_map<std::string, int> _wordtoind;
+        std::mt19937 _gen;
+};
+
+#endif
+
